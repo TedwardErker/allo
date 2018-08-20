7e8707d49c667dc50d98727b5a3268a1cfe27c4b
Tedward Erker <erker@wisc.edu>
2018-08-20 16:45:00 -0500

    Mon Aug 20 16:45:00 CDT 2018

---
+++ code/allo_functions.R
@@ -0,0 +1,46 @@
+## [[file:~/git/allo/code/allo.org::*Libraries%20and%20functions][Libraries and functions:2]]
+generate_formula <- function(model_table, gen, sp, cit, clim, het) {
+      f <- filter(model_table, genus == gen, species == sp, cities == cit, climate == clim, hetero == het)
+      if(f$sigma == "") {
+          bf(formula(f$data), formula(f$b0), formula(f$b1), formula(f$b2), nl = T)
+      } else {
+          bf(f$data, f$b0, f$b1, f$b2, f$sigma, nl = T)
+      }
+  }
+
+  generate_prior <- function(gen, sp, cit, clim) {
+
+      nlprior <- c(prior(gamma(4, 3), nlpar = "b0",lb = 0),
+                   prior(gamma(5, 1), nlpar = "b1", lb = 0),
+                   prior(gamma(4, 3), nlpar = "b2",lb = 0))
+
+      if("many" %in% c(gen, sp, cit)) {
+
+          nlprior <- c(nlprior,
+                       prior(cauchy(0,.1), class = "sd", nlpar = "b0"),
+                       prior(cauchy(0,.1), class = "sd", nlpar = "b1"),
+                       prior(cauchy(0,.1), class = "sd", nlpar = "b2"))
+
+      }
+
+      if(clim == "b0") {
+          nlprior <- c(nlprior,
+                       prior(normal(.3,.25), class = "sds", nlpar = "b0"))
+      }
+
+      if(hetero == "linear") {
+        nlprior <- c(nlprior,
+                     prior(normal(1,.3), class = "b", dpar = "sigma"))
+      }
+
+      if(hetero == "smooth") {
+               nlprior <- c(nlprior,
+                            prior(normal(1,.3), class = "b", dpar = "sigma"),
+                            prior(normal(1,.3), class = "sds", dpar = "sigma"),
+                            prior(normal(0,1), class = "Intercept", dpar = "sigma"))
+}
+
+
+      nlprior
+  }
+## Libraries and functions:2 ends here
--- code/genus_none_species_single_cities_single_climate_none_hetero_linear.R
+++ code/genus_none_species_single_cities_single_climate_none_hetero_linear.R
@@ -1,37 +1,7 @@
 ## [[file:~/git/allo/code/allo.org::*libraries%20and%20functions][libraries and functions:1]]
 library(dplyr)
 library(brms)
-
-generate_formula <- function(model_table, gen, sp, cit, clim, het) {
-    f <- filter(model_table, genus == gen, species == sp, cities == cit, climate == clim, hetero == het)
-    if(f$sigma == "") {
-        bf(formula(f$data), formula(f$b0), formula(f$b1), formula(f$b2), nl = T)
-    } else {
-        bf(f$data, f$b0, f$b1, f$b2, f$sigma, nl = T)
-    }
-}
-
-generate_prior <- function(gen, sp, cit, clim) {
-
-    nlprior <- c(prior(gamma(4, 3), nlpar = "b0",lb = 0),
-                 prior(gamma(5, 1), nlpar = "b1", lb = 0),
-                 prior(gamma(4, 3), nlpar = "b2",lb = 0))
-
-    if("many" %in% c(gen, sp, cit)) {
-
-        nlprior <- c(nlprior,
-                     prior(cauchy(0,.1), class = "sd", nlpar = "b0"),
-                     prior(cauchy(0,.1), class = "sd", nlpar = "b1"),
-                     prior(cauchy(0,.1), class = "sd", nlpar = "b2"))
-
-    }
-
-    if(clim == "b0") {
-        nlprior <- c(nlprior,
-                     prior(normal(.3,.25), class = "sds", nlpar = "b0"))
-    }
-    nlprior
-}
+source("allo_functions.R")
 ## libraries and functions:1 ends here
 
 ## [[file:~/git/allo/code/allo.org::*set%20values][set values:1]]
@@ -39,7 +9,7 @@ genus <- "none"
 species <- "single"
 cities <- "single"
 climate <- "none"
-hetero <- "no"
+hetero <- "linear"
 ## set values:1 ends here
 
 ## [[file:~/git/allo/code/allo.org::*generate%20formula%20and%20priors][generate formula and priors:1]]
