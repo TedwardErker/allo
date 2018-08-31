e07b7aa520dd83a819daabaa663016750a8920ff
Tedward Erker <erker@wisc.edu>
2018-08-30 21:00:02 -0500

    Thu Aug 30 21:00:02 CDT 2018

---
+++ code/genus_none_species_many_cities_single_climate_none_hetero_no_family_Gamma.R
@@ -0,0 +1,27 @@
+## [[file:~/git/allo/code/allo.org::*libraries%20and%20functions][libraries and functions:1]]
+library(dplyr)
+library(brms)
+source("allo_functions.R")
+## libraries and functions:1 ends here
+
+## [[file:~/git/allo/code/allo.org::*set%20values][set values:1]]
+genus <- "none"
+species <- "many"
+cities <- "single"
+climate <- "none"
+hetero <- "no"
+family <- "Gamma"
+## set values:1 ends here
+
+## [[file:~/git/allo/code/allo.org::*generate%20formula%20and%20priors][generate formula and priors:1]]
+form <- generate_formula(genus, species, cities, climate, hetero, family)
+
+nlprior <- generate_prior(genus, species, cities, climate, family)
+## generate formula and priors:1 ends here
+
+## [[file:~/git/allo/code/allo.org::*fit%20model%20to%20real%20data][fit model to real data:1]]
+d <- readRDS("../data/age_dbh_testing.rds")
+
+mod <- brm(form, chains = 4, cores = 4, data = d, init_r = .3, prior = nlprior, iter = 2000, family = Gamma('log'))
+saveRDS(mod, paste0("../models/genus_",genus,"_species_",species,"_cities_", cities, "_climate_", climate, "_hetero_", hetero, ".rds"))
+## fit model to real data:1 ends here
