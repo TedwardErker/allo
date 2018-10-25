8fda4ef70588923ecafd81f92b487b0a88094de3
Tedward Erker <erker@wisc.edu>
2018-10-25 12:00:00 -0500

    Thu Oct 25 12:00:00 CDT 2018

--- code/genus_species_cities_b3climate.R
+++ code/genus_species_cities_b3climate.R
@@ -24,22 +24,22 @@ nlprior <- c(prior(gamma(9, 3), nlpar = "b0",lb = 0),
              prior(gamma(34, 19.4), nlpar = "b1",lb = 0),
              prior(gamma(69.4, 55.5), nlpar = "b2", lb = 0),
              prior(gamma(16, 26), nlpar = "b3", lb = 0),
-             prior(normal(0.01, 0.005), nlpar = "b3", coef = "gdd"),
+             prior(normal(0.01, 0.015), nlpar = "b3", coef = "gdd"),
              prior(normal(0.01, 0.01), nlpar = "b3", coef = "precip"),
-             prior(normal(0.005, 0.0025), nlpar = "b3", coef = "precip:gdd"),
+             prior(normal(0.005, 0.005), nlpar = "b3", coef = "precip:gdd"),
              prior(gamma(20, 1), class = "shape"),
-             prior(normal(0, .1), class = "sd", nlpar = "b0", group = "City"),
-             prior(normal(0, .1), class = "sd", nlpar = "b1", group = "City"),
-             prior(normal(0, .03), class = "sd", nlpar = "b2", group = "City"),
-             prior(normal(0, .03), class = "sd", nlpar = "b3", group = "City"),
-             prior(normal(0, .2), class = "sd", nlpar = "b0", group = "Genus"),
-             prior(normal(0, .05), class = "sd", nlpar = "b0", group = "Genus:Species"),
-             prior(normal(0, .1), class = "sd", nlpar = "b1", group = "Genus"),
+             prior(normal(0, .3), class = "sd", nlpar = "b0", group = "City"),
+             prior(normal(.1, .3), class = "sd", nlpar = "b1", group = "City"),
+             prior(normal(0, .1), class = "sd", nlpar = "b2", group = "City"),
+             prior(normal(0, .1), class = "sd", nlpar = "b3", group = "City"),
+             prior(normal(0, .4), class = "sd", nlpar = "b0", group = "Genus"),
+             prior(normal(0, .1), class = "sd", nlpar = "b0", group = "Genus:Species"),
+             prior(normal(.1, .4), class = "sd", nlpar = "b1", group = "Genus"),
              prior(normal(0, .1), class = "sd", nlpar = "b1", group = "Genus:Species"),
-             prior(normal(0, .05), class = "sd", nlpar = "b2", group = "Genus"),
-             prior(normal(0, .025), class = "sd", nlpar = "b2", group = "Genus:Species"),
-             prior(normal(0, .03), class = "sd", nlpar = "b3", group = "Genus"),
-             prior(normal(0, .025), class = "sd", nlpar = "b3", group = "Genus:Species"))
+             prior(normal(0, .1), class = "sd", nlpar = "b2", group = "Genus"),
+             prior(normal(0, .05), class = "sd", nlpar = "b2", group = "Genus:Species"),
+             prior(normal(0, .1), class = "sd", nlpar = "b3", group = "Genus"),
+             prior(normal(0, .05), class = "sd", nlpar = "b3", group = "Genus:Species"))
 
 
 d <- readRDS("../data/age_dbh_testing_noWARO.rds")
@@ -57,7 +57,7 @@ mod <- brm(form,
            data = d,
            prior = nlprior,
            family = Gamma("identity"),
-           chains = 12, cores = 12, init_r = .3, iter = 500)
+           chains = 12, cores = 12, init_r = .3, iter = 6000, control = list(adapt_delta = .9))
 
-saveRDS(mod, paste0("../models/genus_",genus,"_species_",species,"_cities_", cities, "_climate_", climate, "_hetero_", hetero, "_family_", family, "tighter_priors.rds"))
+saveRDS(mod, paste0("../models/genus_",genus,"_species_",species,"_cities_", cities, "_climate_", climate, "_hetero_", hetero, "_family_", family, ".rds"))
 ## model R code:1 ends here
