dbf3077ffab77d9fd9fe2a0593f3d5878239d5da
Tedward Erker <erker@wisc.edu>
2018-10-26 20:00:00 -0500

    Fri Oct 26 20:00:00 CDT 2018

--- code/marginal_effects_fullconditions.R
+++ code/marginal_effects_fullconditions.R
@@ -1,4 +1,4 @@
-## [[file:~/git/allo/code/allo.org::*assess%20model][assess model:11]]
+## [[file:~/git/allo/code/allo.org::*assess%20model][assess model:18]]
 library(brms)
 library(dplyr)
 mod <- readRDS("../models/genus_many_species_many_cities_many_climate_b3linint_hetero_no_family_Gamma.rds")
@@ -8,4 +8,16 @@ mod <- readRDS("../models/genus_many_species_many_cities_many_climate_b3linint_h
 
      me <- marginal_effects(mod, effects = "AGE", conditions = cond, re_formula = NULL, method = "predict")
 saveRDS(me, "../models/genus_many_species_many_cities_many_climate_b3linint_hetero_no_family_Gamma_marginaleffects.rds")
-## assess model:11 ends here
+## assess model:18 ends here
+
+## [[file:~/git/allo/code/allo.org::*assess%20model][assess model:18]]
+library(brms)
+library(dplyr)
+mod <- readRDS("../models/genus_many_species_many_cities_many_climate_b3linint_hetero_no_family_Gamma.rds")
+  cond <- expand.grid(Species = unique(mod$data$Species),  City = unique(mod$data$City))
+    cond <- left_join(cond, unique(select(mod$data, Species, Genus)))
+    cond <- left_join(cond, unique(select(mod$data, City, precip, gdd)))
+
+     me <- marginal_effects(mod, effects = "AGE", conditions = cond, re_formula = NULL, method = "predict")
+saveRDS(me, "../models/genus_many_species_many_cities_many_climate_b3linint_hetero_no_family_Gamma_marginaleffects.rds")
+## assess model:18 ends here
