56add66b7077e27d087d65ac3eaa4fe479702bf6
Tedward Erker <erker@wisc.edu>
2018-11-27 14:30:00 -0600

    Tue Nov 27 14:30:00 CST 2018

--- code/calc_marginal_effects_fulldata.R
+++ code/calc_marginal_effects_fulldata.R
@@ -11,6 +11,6 @@ cond <- expand.grid(Species = unique(mod$data$Species),  City = unique(mod$data$
 cond <- left_join(cond, unique(select(mod$data, Species, Genus)))
 cond <- left_join(cond, unique(select(mod$data, City, precip, gdd)))
 
-me <- marginal_effects(mod, effects = "AGE", conditions = cond, re_formula = NULL, method = "predict", nsamples = 1000)
+me <- marginal_effects(mod, effects = "AGE", conditions = cond, re_formula = NULL, method = "predict", nsamples = 5000)
 saveRDS(me, "../models/genus_many_species_many_cities_notB1_many_climate_b3linint_hetero_no_family_Gamma_FullData_marginaleffects.rds")
 ## assess model:14 ends here
