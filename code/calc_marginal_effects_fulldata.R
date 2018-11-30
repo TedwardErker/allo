1c17df37664c48075a0b064f1ec92f0639d8742d
Tedward Erker <erker@wisc.edu>
2018-11-30 16:15:00 -0600

    Fri Nov 30 16:15:00 CST 2018

--- code/calc_marginal_effects_fulldata.R
+++ code/calc_marginal_effects_fulldata.R
@@ -7,11 +7,11 @@ library(dplyr)
  ## saveRDS(precip.gdd, "../models/genus_many_species_many_cities_many_notB1_climate_b3linint_hetero_no_family_Gamma_FullData_precip.gdd.surface.rds")
 
 
- cond <- expand.grid(Genus = unique(mod$data$Genus)) #City = unique(mod$data$City)) #Species = unique(mod$data$Species))#,  
+ cond <- expand.grid(City = unique(mod$data$City), Species = unique(mod$data$Species))
 
-#cond <- left_join(cond, unique(select(mod$data, Species, Genus)))
-# cond <- left_join(cond, unique(select(mod$data, City, precip, gdd)))
+cond <- left_join(cond, unique(select(mod$data, Species, Genus)))
+ cond <- left_join(cond, unique(select(mod$data, City, precip, gdd)))
 
- me <- marginal_effects(mod, effects = "AGE", conditions = cond, re_formula = NULL, method = "fitted", nsamples = 15000)
- saveRDS(me, "../models/genus_many_species_many_cities_notB1_many_climate_b3linint_hetero_no_family_Gamma_FullData_marginaleffects_Genus_methodfitted.rds")
+ me <- marginal_effects(mod, effects = "AGE", conditions = cond, re_formula = NULL, method = "fitted", nsamples = 1000)
+ saveRDS(me, "../models/genus_many_species_many_cities_notB1_many_climate_b3linint_hetero_no_family_Gamma_FullData_marginaleffects_methodfitted.rds")
 ## assess model:14 ends here
